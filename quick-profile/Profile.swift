3f684d4df4d96ff9b1a08a79b89c886e1a58270a
Kendall Aubertot <kendall.aubertot@gmail.com>
2015-07-09 10:33:29 -0600

    Add Profile model to project.
    This model file is so far unused. Don't get too excited.

---
+++ quick-profile/Profile.swift
@@ -0,0 +1,13 @@
+//
+//  Profile.swift
+//  quick-profile
+//
+//  Created by Kendall Aubertot on 7/9/15.
+//  Copyright (c) 2015 kndl. All rights reserved.
+//
+
+import UIKit
+
+class Profile: NSObject {
+   
+}

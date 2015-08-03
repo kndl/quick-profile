04a0c11af23ca0e7721329dd10ef1f9988bde9e6
Kendall Aubertot <kendall.aubertot@gmail.com>
2015-08-02 23:57:53 -0600

    Launch the app without the Storyboard.
    Remove the Storyboard. Edit the Info.plist to prevent the app from using the
    plist when launching. Update the app delegate entrance method to create the
    initial window.

--- quick-profile/AppDelegate.swift
+++ quick-profile/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
         let controller = RegistrationViewController()
         let navigationController = UINavigationController(rootViewController: controller)
         window?.rootViewController = navigationController
